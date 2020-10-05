6b4e5809601ce3441ec393cf2f5906ae592ba2dc
kevinwksun <61125768+kevinwksun@users.noreply.github.com>
2020-10-04 22:06:09 -0700

    updated 'work' to 'index'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+console.log("Your index.js file is loaded correctly!")
+  ;
+
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
